304de84b72e7d583c38c431be1b5ebdb07fbe4b7
Prasanna Swaminathan <pswaminathan@users.noreply.github.com>
2019-12-11 14:03:07 -0500

    Use value receiver for MarshalText (#37)
    * Use value receiver for MarshalText
    
    * Switch to same version of go-bindata installed by homebrew

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ type rootT struct {
 	FileNames      []string `cli:"*f,file" usage:"The file(s) to generate enums.  Use more than one flag for more files."`
 	NoPrefix       bool     `cli:"noprefix" usage:"Prevents the constants generated from having the Enum as a prefix."`
 	Lowercase      bool     `cli:"lower" usage:"Adds lowercase variants of the enum strings for lookup."`
-	Marshal        bool     `cli:"marshal" usage:"Adds text marshalling functions."`
+	Marshal        bool     `cli:"marshal" usage:"Adds text (and inherently json) marshalling functions."`
 	SQL            bool     `cli:"sql" usage:"Adds SQL database scan and value functions."`
 	Flag           bool     `cli:"flag" usage:"Adds golang flag functions."`
 	Prefix         string   `cli:"prefix" usage:"Replaces the prefix with a user one."`
