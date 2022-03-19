b21c8be6af08021b06ca784295ea659d9a3ad251
Alex Bice <alex.bice@rewardstyle.com>
2022-03-19 15:45:10 -0700

    support 64 bit types

--- generator/example_test.go
+++ generator/example_test.go
@@ -102,3 +102,23 @@ Green = 33 // Green starts with 33
 // red-orange // has a , in it!?!
 // )
 type ColorWithComment4 int
+
+/* ENUM(
+Unknown= 0
+E2P15					= 32768
+E2P16					= 65536
+E2P17					= 131072
+E2P18					= 262144
+E2P19					= 524288
+E2P20					= 1048576
+E2P21					= 2097152
+E2P22					= 33554432
+E2P23					= 67108864
+E2P28					= 536870912
+E2P30					= 1073741824
+E2P31					= 2147483648
+E2P32					= 4294967296
+E2P33					= 8454967296
+)
+*/
+type Enum64bit uint64
